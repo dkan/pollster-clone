0b7a9cbdede7104bf3380ad789c4cfc1770ab8ab
Damien <damien.kan@gmail.com>
2012-07-30 10:50:43 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PollsterClone::Application.config.secret_token = '0a9fdabf899f5db13b2d4b80c79e61b1a34bf75e51b4c130b9594d975ed8099fa86d3ca2d549a9219af7e905bda61686427a6d245aad41ada08b683128606445'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PollsterClone::Application.config.session_store :cookie_store, key: '_pollster_clone_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PollsterClone::Application.config.session_store :active_record_store
